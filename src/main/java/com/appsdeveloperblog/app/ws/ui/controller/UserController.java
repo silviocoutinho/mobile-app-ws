1d9cc05bffd1605dc32165b0f1ddea1f7a37587d
silviocoutinho <silviocoutinho@ymail.com>
2021-08-04 22:53:09 -0300

    Reading Path Annotation in User

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -2,18 +2,25 @@ package com.appsdeveloperblog.app.ws.ui.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
-	
+
 	@GetMapping
-	public String getUser() {
-		return "get user was called";
+	public String getUsers(@RequestParam(value="page") int page, @RequestParam(value="limit") int limit) {
+		return "get user was called with page = " + page + " and limit = " + limit;
+	}
+	
+	@GetMapping(path="/{userId}")
+	public String getUser(@PathVariable String userId) {
+		return "get user was called with userId = " + userId;
 	}
 	
 	@PostMapping
