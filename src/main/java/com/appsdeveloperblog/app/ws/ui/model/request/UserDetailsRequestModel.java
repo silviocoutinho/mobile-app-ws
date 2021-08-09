13910a9fc14d45c652849b67529ec490dcf9169c
Silvio Coutinho <silviocoutinho@ymail.com>
2021-08-09 18:56:40 -0300

    Config Valid Annotations

--- src/main/java/com/appsdeveloperblog/app/ws/ui/model/request/UserDetailsRequestModel.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/model/request/UserDetailsRequestModel.java
@@ -9,11 +9,14 @@ public class UserDetailsRequestModel {
 	@NotNull(message = "First name cannot be missing or empty")
 	@Size(min = 2, message = "First name must not be less than 2 characters")
 	private String firstName;
+
 	@NotNull(message = "Last name cannot be missing or empty")
 	@Size(min = 2, message = "Last name must not be less than 2 characters")
 	private String lastName;
+
 	@Email
 	private String email;
+
 	@NotNull(message = "Password is a required field")
 	@Size(min = 8, max = 16, message = "Password must be equal to or greater than 8 characters and less than 16 characters")
 	private String password;
