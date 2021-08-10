02d8c80fa401581d9b54481616feb4a1623b6bff
Silvio Coutinho <silviocoutinho@ymail.com>
2021-08-10 08:54:46 -0300

    Add Delete method in users

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -100,9 +100,10 @@ public class UserController {
 		return storedUserDetails;
 	}
 	
-	@DeleteMapping
-	public String deleteUser() {
-		return "delete user was called";
+	@DeleteMapping(path="/{id}")
+	public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+		users.remove(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 		
