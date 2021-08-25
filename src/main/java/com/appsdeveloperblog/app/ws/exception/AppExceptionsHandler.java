fb9ccbbdff4236415549a16c72c2f352eddb5f0e
silviocoutinho <silviocoutinho@ymail.com>
2021-08-24 23:11:05 -0300

    Two Exception with one Method

--- src/main/java/com/appsdeveloperblog/app/ws/exception/AppExceptionsHandler.java
+++ src/main/java/com/appsdeveloperblog/app/ws/exception/AppExceptionsHandler.java
@@ -29,8 +29,8 @@ public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
 	}
 	
 
-	@ExceptionHandler(value = { NullPointerException.class })
-	public ResponseEntity<Object> handleNullPointerException (NullPointerException ex, WebRequest request) {
+	@ExceptionHandler(value = { NullPointerException.class, UserServiceException.class })
+	public ResponseEntity<Object> handleNullPointerException (Exception ex, WebRequest request) {
 		
 		String errorMessageDescription = ex.getLocalizedMessage();
 		
@@ -41,16 +41,6 @@ public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
 		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	@ExceptionHandler(value = { UserServiceException.class })
-	public ResponseEntity<Object> handleUserServiceException (UserServiceException ex, WebRequest request) {
-		
-		String errorMessageDescription = ex.getLocalizedMessage();
-		
-		if (errorMessageDescription == null) errorMessageDescription = ex.toString();
 
-		ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
-
-		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
-	}
 	
 }
